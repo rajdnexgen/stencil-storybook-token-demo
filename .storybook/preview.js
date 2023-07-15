8abde78ffb063f582ae10176248ebb4482883312
Khader Vali <khader.kalava@telepo.com>
2023-07-15 20:06:49 +0200

    fixes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,79 @@
 import { html } from 'lit-html';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import {
+  defineCustomElements,
+  applyPolyfills,
+} from '../loader';
+
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+  },
+  a11y: {
+    config: {
+      rules: [
+        {
+          id: 'list',
+          enabled: false,
+        },
+        {
+          id: 'listitem',
+          enabled: false,
+        },
+        {
+          id: 'color-contrast',
+          enabled: false,
+        },
+      ],
+    },
+  },
+  viewMode: 'docs',
+  options: {
+    storySort: {
+      order: [
+        'Scale Design System',
+        'About Scale',
+        'FAQ',
+        'New Release',
+        ['Release Notes', 'Sketch Library Update'],
+        'Setup & Info',
+        [
+          'Getting Started For Designers',
+          'Getting Started For Developers',
+          'Scale and Vue',
+          'Scale and Angular',
+          'Scale and React',
+          'Accessibility',
+          'Browser Support',
+        ],
+        'Guidelines',
+        [
+          'Design Tokens',
+          'Light and Dark Mode',
+          'Customization and Themes',
+          'Grid',
+          'Spacings',
+          'Typography',
+          'Colors',
+          'Shadows',
+        ],
+        'Update History',
+        ['Design', 'Development'],
+        'Components',
+        'Beta Components',
+        'Deprecated Components',
+        'Contact',
+        'Legal',
+        ['Disclaimer', 'Legal Notice', 'Privacy'],
+      ],
+    },
+  },
 };
 
 export const globalTypes = {
